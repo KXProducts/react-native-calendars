de5fc52030605b7f19382ff60c17ca26cb5cb425
Oystein Borg <oystein.borg@spoortz.no>
2019-01-15 14:33:57 +0100

    Use standard sizes

--- src/agenda/index.js
+++ src/agenda/index.js
@@ -10,7 +10,7 @@ import ReservationsList from './reservation-list';
 import styleConstructor from './style';
 import { VelocityTracker } from '../input';
 
-const HEADER_HEIGHT = 88;
+const HEADER_HEIGHT = 104;
 const KNOB_HEIGHT = 24;
 
 //Fallback when RN version is < 0.44
